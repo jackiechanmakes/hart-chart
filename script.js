d1d6bcc34be15084accc7181ab824e65222920c0
jackiechanmakes <jackiechanmakes@gmail.com>
2023-07-16 08:45:34 -0700

    Refactor code using DRY principle

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
+let table = document.getElementById("table");
+
+// Generate Hart Chart
 let refreshButton = document.getElementById("refresh-button");
 refreshButton.addEventListener("click", function() {
-    let color = document.getElementById("color");
-    let colorValue = color.value;
+    let colors = document.getElementById("color");
+    let colorsValue = colors.value;
 
     let content = document.getElementById("content");
     let contentValue = content.value;
 
-    if (colorValue === "black-white" && contentValue === "letters") {
-        getAlphaCharsBlackWhite();
-    } else if (colorValue === "black-white" && contentValue === "letters-numbers") {
-        getAlphaNumericCharsBlackWhite();
-    } else if (colorValue === "multicolor-2" && contentValue === "letters") {
-        getAlphaCharsMulticolor2("red", "green");
-    } else if (colorValue === "multicolor-2" && contentValue === "letters-numbers") {
-        getAlphaNumericCharsMulticolor2("red", "green");
-    } else if (colorValue === "multicolor-3" && contentValue === "letters") {
-        getAlphaCharsMulticolor3("red", "green", "blue");
-    } else if (colorValue === "multicolor-3" && contentValue === "letters-numbers") {
-        getAlphaNumericCharsMulticolor3("red", "green", "blue");
+    for (let i = 0, row; row = table.rows[i]; i++) {
+        for (let j = 0, col; col = row.cells[j]; j++) {
+            col.innerText = getCharacter(contentValue);
+            col.style.color = getColor(colorsValue);
+        }
     }
 })
 
-let table = document.getElementById("table");
+function getCharacter(content) {
+    let alphaChars = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+    let numericChars = "1234567890";
+    let alphaNumericChars = alphaChars + numericChars;
 
+    if (content === "" || content === "letters") {
+        return alphaChars.charAt(Math.floor(Math.random() * alphaChars.length));
+    } else if (content === "letters-numbers") {
+        return alphaNumericChars.charAt(Math.floor(Math.random() * alphaNumericChars.length));
+    }
+}
+
+function getColor(colors) {
+    let colorOptions;
+
+    if (colors === "" || colors === "black-white") {
+        colorOptions = ["black"];
+    } else if (colors === "multicolor-2") {
+        colorOptions = ["red", "green"];
+    } else if (colors === "multicolor-3") {
+        colorOptions = ["red", "green", "blue"];
+    }
+
+    let randomChoice = Math.floor(Math.random() * colorOptions.length);
+    return colorOptions[randomChoice];
+}
+
+// Download PDF of generated Hart Chart
 let downloadButton = document.getElementById("download-button");
 downloadButton.addEventListener("click", function() {
     let opt = {
@@ -34,91 +56,4 @@ downloadButton.addEventListener("click", function() {
       };
 
     html2pdf().set(opt).from(table).save();
-
 });
-
-function getAlphaCharsBlackWhite() {
-    let letters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase();
-
-    for (let i = 0, row; row = table.rows[i]; i++) {
-        for (let j = 0, col; col = row.cells[j]; j++) {
-            col.innerText = letters.charAt(Math.floor(Math.random() * letters.length));
-            col.style.color = "black";
-        }
-    }
-}
-
-function getAlphaNumericCharsBlackWhite() {
-    let letters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase();
-    let nums = '1234567890'
-    let alphaNumericChars = letters + nums;
-
-    for (let i = 0, row; row = table.rows[i]; i++) {
-        for (let j = 0, col; col = row.cells[j]; j++) {
-            col.innerText = alphaNumericChars.charAt(Math.floor(Math.random() * alphaNumericChars.length));
-            col.style.color = "black";
-        }
-    }
-}
-
-function getAlphaCharsMulticolor2(color1, color2) {
-    let letters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase();
-
-    for (let i = 0, row; row = table.rows[i]; i++) {
-        for (let j = 0, col; col = row.cells[j]; j++) {
-            col.innerText = letters.charAt(Math.floor(Math.random() * letters.length));
-            Math.random() < 0.5 ? col.style.color = color1 : col.style.color = color2;
-        }
-    }
-}
-
-function getAlphaNumericCharsMulticolor2(color1, color2) {
-    let letters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase();
-    let nums = '1234567890'
-    let alphaNumericChars = letters + nums;
-
-    for (let i = 0, row; row = table.rows[i]; i++) {
-        for (let j = 0, col; col = row.cells[j]; j++) {
-            col.innerText = alphaNumericChars.charAt(Math.floor(Math.random() * alphaNumericChars.length));
-            Math.random() < 0.5 ? col.style.color = color1 : col.style.color = color2;
-        }
-    }
-}
-
-function getAlphaCharsMulticolor3(color1, color2, color3) {
-    let letters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase();
-
-    for (let i = 0, row; row = table.rows[i]; i++) {
-        for (let j = 0, col; col = row.cells[j]; j++) {
-            col.innerText = letters.charAt(Math.floor(Math.random() * letters.length));
-            let randomChoice = Math.floor(Math.random() * 3);
-            if (randomChoice === 1) {
-                col.style.color = color1;
-            } else if (randomChoice === 2) {
-                col.style.color = color2;
-            } else {
-                col.style.color = color3;
-            }
-        }
-    }
-}
-
-function getAlphaNumericCharsMulticolor3(color1, color2, color3) {
-    let letters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase();
-    let nums = '1234567890'
-    let alphaNumericChars = letters + nums;
-
-    for (let i = 0, row; row = table.rows[i]; i++) {
-        for (let j = 0, col; col = row.cells[j]; j++) {
-            col.innerText = alphaNumericChars.charAt(Math.floor(Math.random() * alphaNumericChars.length));
-            let randomChoice = Math.floor(Math.random() * 3);
-            if (randomChoice === 1) {
-                col.style.color = color1;
-            } else if (randomChoice === 2) {
-                col.style.color = color2;
-            } else {
-                col.style.color = color3;
-            }
-        }
-    }
-}
