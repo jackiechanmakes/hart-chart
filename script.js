e285dae3f4fac000fa81f5b14aa2f5bb4ceb01cf
jackiechanmakes <jackiechanmakes@gmail.com>
2023-07-15 17:12:52 -0700

    Improve download pdf canvas sizing

--- script.js
+++ script.js
@@ -5,19 +5,38 @@ let table = document.getElementById("table");
 
 let downloadButton = document.getElementById("download-button");
 downloadButton.addEventListener("click", function() {
-    let mywindow = window.open("", "PRINT",
-                "height=400,width=600");
+    let opt = {
+        margin:       [90, 0],
+        filename:     'test.pdf',
+        // image:        { type: 'jpeg', quality: 1.00 },
+        html2canvas:  { scale: 1, width: 1850, height: 900},
+        jsPDF:        { unit: 'px', format: 'letter', orientation: 'landscape' }
+      };
 
-    mywindow.document.write(table.innerHTML);
+    html2pdf().set(opt).from(table).to('img').save();
 
-    mywindow.document.close();
-    mywindow.focus();
-
-    mywindow.print();
-    mywindow.close();
-
-    return true;
 });
+
+// let button = document.getElementById("download-button");
+// button.addEventListener("click", function () {
+//     // let doc = new jsPDF("p", "mm", [300, 300]);
+//         let doc = new jsPDF("p", "pt", 'a4');
+
+//         doc.html(document.querySelector("#table"), {
+//             callback: function(doc) {
+//                 doc.save("HartChart.pdf");
+//             },
+//             margin: [60, 60, 60, 60],
+//             x: 30,
+//             y: 30
+//         });
+//     // let makePDF = document.querySelector("#table");
+
+//     // fromHTML Method
+//     // doc.fromHTML(makePDF);
+//     // doc.save("output.pdf");
+// });
+
 
 function getAlphaChars() {
     let letters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase();
